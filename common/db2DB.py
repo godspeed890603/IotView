7ba0f0bacc1f9dfe599e5b19d627fd3f1e27303a
kermit.chen <kermit.chen@gmail.com>
2024-09-21 17:35:21 +0800

    2024/09/21 add db2 & sqliteDB py file

---
+++ common/db2DB.py
@@ -0,0 +1,22 @@
+
+def insert():
+    print()
+
+def update():
+    print()
+
+def delete():
+    print()
+
+def select():
+    print()
+
+def connect():
+    print()
+
+def close():
+    print()
+
+
+if __name__ == "__main__":
+    print()
