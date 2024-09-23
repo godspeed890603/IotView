5e9ca57703a2c8f6e971b5d0ee62a08da1b2e84e
kermit.chen <kermit.chen@gmail.com>
2024-09-23 20:05:47 +0800

    2024/09/22 add 物件導向版本 & reconnect功能

--- service/service1.py
+++ service/service1.py
@@ -109,29 +109,7 @@ def main():
         print(f"act_crr_id: {act_crr_id}")
         print("-----------")
 
-    #   # 反转义字符串，使其成为有效的 JSON 字符串
-    #     unescaped_payload = payload.encode('utf-8').decode('unicode_escape')
-        
-    #     print(f"Unescaped Payload: {unescaped_payload}")
-        
-    #     # 去除引号包裹，如果有的话
-    #     if unescaped_payload.startswith('"') and unescaped_payload.endswith('"'):
-    #         unescaped_payload = unescaped_payload[1:-1]
-        
-    #     # 将反转义后的字符串解析为 Python 字典
-    #     data1 = json.loads(unescaped_payload)
-    #     # 訪問數據
-    #     x_acc = data['data']['x_acc']
-    #     max_x_acc = data['data']['max_x_acc']
-    #     y_acc = data['data']['y_acc']
-    #     max_y_acc = data['data']['max_y_acc']
-    #     z_acc = data['data']['z_acc']
-    #     max_z_acc = data['data']['max_z_acc']
 
-    #     # 輸出結果
-    #     print(f"x_acc: {x_acc}, max_x_acc: {max_x_acc}")
-    #     print(f"y_acc: {y_acc}, max_y_acc: {max_y_acc}")
-    #     print(f"z_acc: {z_acc}, max_z_acc: {max_z_acc}")
     # 解析 JSON 字符串为 Python 字典
         data = json.loads(payload)
 
